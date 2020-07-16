82dadf261f4609cceab7668cfc3399b1f1616274
unknown <Iceversaka@hotmail.com>
2020-07-15 19:02:15 -0500

    Starting over yet again, this time I think we got it right.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
