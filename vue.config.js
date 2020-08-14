6051ac646472f5cca7fd241a25acf7a48b459dd8
Seth Hollingsead <Iceversaka@hotmail.com>
2020-08-14 16:43:19 -0500

    Dev: Update trying to fix the NSIS configuration for the Electron-Builder.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.SethEden.electronVueJS-App",
+        productName: "Electron VueJS App",
+        win: {
+          target: [ "nsis" ]
+        }
+      }
+    }
+  }
 }
